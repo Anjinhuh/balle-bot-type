04701dc437f2688b23a7f22c8a4d7eac515d6982
Anjinho <65797644+Anjinhuh@users.noreply.github.com>
2021-12-23 23:27:20 -0300

    Está faltando apenas a responsividade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import logo from './img/logo.svg';
+import botVector from './img/bot.svg'
+import balle from './img/BALLERINI.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="header-logo">
+          <img src={logo} alt='Logo BALLERINI' />
+        </div>
+        <div className="header-text">
+          <p>Comunidade Ballerini</p>
+          <p>Tutorial</p>
+          <p>Open Source</p>
+          <p>Comandos</p>
+        </div>
       </header>
+      
+
+      <div className="item-landing-page">
+        <div className='fix-visual-errors'>
+          <p className="text-principal">Balle Bot</p>
+          <p className="text-secondary">Deixe a sua comunidade segura de um jeito fácil</p>
+          <button className="btn-add">Me adicione</button>
+        </div>
+        <div>
+          <img src ={botVector} alt='bot' />
+        </div>
+      </div>
+
+      <div className="line-bottom-header"></div>
+
+      <div className="details-bot">
+        <p className="text-primary">O que ela faz por você?</p>
+        <p className="text-secondary">1. Cria um sistema de verificação capcha para novos membros garantirem que não são robôs</p>
+        <p className="text-secondary">2. Possui um sistema de banimento automático para links suspeitos que forem enviados na comunidade</p>
+        <p className="text-secondary">3. Sistema de avisos e banimento manual, para pessoas administradoras utilizarem</p>
+      </div>
+      <div className="footer-balle">
+        <img src={balle} alt='balle' />
+      </div>
     </div>
   );
 }
